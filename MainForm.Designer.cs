a500cce7bfde5f372a263e29510cb47773b7cb56
Savage-E <Savage-E@users.noreply.github.com>
2020-11-25 17:25:11 +0300

    initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace DataBase
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this._folderOpenBtn = new System.Windows.Forms.Button();
+            this.dataGridViewMain = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _folderOpenBtn
+            // 
+            this._folderOpenBtn.Location = new System.Drawing.Point(12, 373);
+            this._folderOpenBtn.Name = "_folderOpenBtn";
+            this._folderOpenBtn.Size = new System.Drawing.Size(152, 55);
+            this._folderOpenBtn.TabIndex = 0;
+            this._folderOpenBtn.Text = "Открыть папку с файлами";
+            this._folderOpenBtn.UseVisualStyleBackColor = true;
+            this._folderOpenBtn.Click += new System.EventHandler(this._folderOpenBtn_Click);
+            // 
+            // dataGridViewMain
+            // 
+            this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMain.Location = new System.Drawing.Point(328, 12);
+            this.dataGridViewMain.Name = "dataGridViewMain";
+            this.dataGridViewMain.RowHeadersWidth = 51;
+            this.dataGridViewMain.RowTemplate.Height = 24;
+            this.dataGridViewMain.Size = new System.Drawing.Size(450, 150);
+            this.dataGridViewMain.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewMain);
+            this.Controls.Add(this._folderOpenBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "DataBase";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button _folderOpenBtn;
+        private System.Windows.Forms.DataGridView dataGridViewMain;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataBase
+{
+    public partial class MainForm : Form
+    {
+        private string openFileName, folderName;
+
+        private bool fileOpened = false;
+        public MainForm()
+        {
+            InitializeComponent();
+            DataGridVievLoad();
+        }
+
+
+        private void DataGridVievLoad()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string[] lines = File.ReadAllLines(@"F:\repos\DataBase\Resources\References.csv");
+                if (lines.Length > 0)
+                {
+                    //first line to create header
+                    string firstLine = lines[0];
+                    string[] headerLabels = firstLine.Split(';');
+                    foreach (string headerWord in headerLabels)
+                    {
+                        dt.Columns.Add(new DataColumn(headerWord));
+                    }
+
+                    //For Data
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+                        string[] dataWords = lines[i].Split(';');
+                        DataRow dr = dt.NewRow();
+                        int columnIndex = 0;
+
+                        foreach (string headerWord in headerLabels)
+                        {
+                            dr[headerWord] = dataWords[columnIndex++];
+                        }
+
+                        dt.Rows.Add(dr);
+                    }
+                }
+
+                if (dt.Rows.Count > 0)
+                {
+                    dataGridViewMain.DataSource = dt;
+                    dataGridViewMain.Columns[0].ReadOnly = true;
+                    dataGridViewMain.Columns[1].ReadOnly = true;
+                    dataGridViewMain.Columns[2].ReadOnly = false;
+                    dataGridViewMain.AllowUserToAddRows = false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Не удалось найти файл References.csv");
+            }
+        }
+
+        private void _folderOpenBtn_Click(object sender, EventArgs e)
+        {
+            // Show the FolderBrowserDialog.
+            var result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderName = folderBrowserDialog1.SelectedPath;
+
+
+                var fileList = Directory.GetFiles(folderName, " *.mhtml");
+
+                ParseFiles(fileList);
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            StringBuilder fileCSV = new StringBuilder("Title;filePath;Comments\t\n");
+            int rows = dataGridViewMain.RowCount - 1;
+            int columns = dataGridViewMain.ColumnCount;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    fileCSV.Append(dataGridViewMain[j, i].Value + ";");
+                }
+
+                if (i == (rows - 1))
+                {
+                    fileCSV.Append("\t");
+                }
+                else
+                {
+                    fileCSV.Append("\t\n");
+                }
+            }
+
+            StreamWriter wr = new StreamWriter(@"F:\repos\DataBase\Resources\References.csv", false,
+                Encoding.GetEncoding("windows-1251"));
+            wr.Write(fileCSV);
+            wr.Close();
+        }
+
+        private void PrintDataToDGV()
+        {
+
+        }
+
+        private void ParseFiles(string[] files)
+        {
+
+
+        }
+
+    }
+}
